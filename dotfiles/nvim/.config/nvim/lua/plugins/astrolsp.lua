41c4304f4ad7e2a9017f7377accba47ab0e28c71
Martin Kröner <work@kroner.dev>
2025-02-28 20:55:52 +0100

    nvim: rust-analyzer: separate cargo profile to avoid locking

--- dotfiles/nvim/.config/nvim/lua/plugins/astrolsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/astrolsp.lua
@@ -70,6 +70,16 @@ return {
           },
         },
       },
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              extraEnv = { CARGO_PROFILE_RUST_ANALYZER_INHERITS = "dev" },
+              extraArgs = { "--profile", "rust-analyzer" },
+            },
+          },
+        },
+      },
     },
     -- customize how language servers are attached
     handlers = {
