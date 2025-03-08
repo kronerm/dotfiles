a5c0f40d045e664971c70c093c4dfa878ec4eb87
Martin Kröner <work@kroner.dev>
2025-03-08 17:19:10 +0100

    fish: improve `ls` alias

--- dotfiles/fish/.config/fish/conf.d/99-interactive.fish
+++ dotfiles/fish/.config/fish/conf.d/99-interactive.fish
@@ -2,7 +2,7 @@ if status is-interactive
     alias cp="cp -i"
     alias df="df -h"
     alias ip='ip -c'
-    alias ls='exa -alH --group-directories-first'
+    alias ls='eza --all --long --links --group-directories-first'
     alias mv='mv -i'
     alias rm='rm -I'
     alias vim='nvim'
