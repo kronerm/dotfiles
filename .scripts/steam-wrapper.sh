9cf0d9ff604174b245e27033a900cf5a0bd5d783
Martin Kröner <work@kroner.dev>
2025-06-13 15:49:18 +0200

    scripts: steam-wrapper.sh: higher values first

--- .scripts/steam-wrapper.sh
+++ .scripts/steam-wrapper.sh
@@ -6,8 +6,8 @@ gamescope=""
 
 if [ "${GAMESCOPE_DISABLED:-"0"}" != "1" ]; then
   available_modes_with_refresh_rates_json="$(wlr-randr --json | jq '[.[].modes[]] | group_by(.width, .height) | map({"\(.[0].width)x\(.[0].height)": map(.refresh)}) | add')"
-  selected_display_resolution="$(echo -e "$available_modes_with_refresh_rates_json" | jq -r 'keys[]' | sort -h | rofi -dmenu)"
-  selected_display_refresh_rate="$(echo -e "$available_modes_with_refresh_rates_json" | jq -r ".\"$selected_display_resolution\"[]" | sort -h | rofi -dmenu)"
+  selected_display_resolution="$(echo -e "$available_modes_with_refresh_rates_json" | jq -r 'keys[]' | sort -hr | rofi -dmenu)"
+  selected_display_refresh_rate="$(echo -e "$available_modes_with_refresh_rates_json" | jq -r ".\"$selected_display_resolution\"[]" | sort -hr | rofi -dmenu)"
 
   selected_display_resolution_width="$(echo "$selected_display_resolution" | cut -d'x' -f1)"
   selected_display_resolution_height="$(echo "$selected_display_resolution" | cut -d'x' -f2)"
